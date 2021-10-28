8a9e21cd0cbd70040fa2ae72df18c9700e6da74c
EvgenyDegtyarev <e.degtyarev@icloud.com>
2021-10-28 18:56:12 +0300

    StoryBoard build
    StoryBoard build

---
+++ Millionaire/Data/GameResult.swift
@@ -0,0 +1,15 @@
+//
+//  GameResult.swift
+//  Millionaire
+//
+//  Created by Евгений Дегтярев on 28.10.2021.
+//
+
+import Foundation
+
+struct ResultOfGame {
+    var gameDate: String
+    var score: Int
+    var percentTrue: Double
+    var countRequest: Int
+}
---
+++ Millionaire/Data/GameSession.swift
@@ -0,0 +1,17 @@
+//
+//  GameSession.swift
+//  Millionaire
+//
+//  Created by Евгений Дегтярев on 28.10.2021.
+//
+
+import Foundation
+import UIKit
+
+class gameSession {
+    var data : Int = 0
+    var trueAnswer: Int = 0
+    var numberQuestion: Int = 0
+   
+}
+
